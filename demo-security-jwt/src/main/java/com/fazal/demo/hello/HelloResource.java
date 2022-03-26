4450109afa6ec350a51144472866d87ab5164789
rehaman <fazalul.rehaman@nokia.com>
2022-03-26 17:13:13 +0530

    Spring boot security and jwt

---
+++ demo-security-jwt/src/main/java/com/fazal/demo/hello/HelloResource.java
@@ -0,0 +1,55 @@
+package com.fazal.demo.hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fazal.demo.hello.model.AuthenticationRequest;
+import com.fazal.demo.hello.model.AuthenticationResponse;
+import com.fazal.demo.security.util.JwtUtil;
+
+@RestController
+public class HelloResource {
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Autowired
+	private JwtUtil jwtUtil;
+
+	@GetMapping({ "/hello" })
+	public String hello() {
+		return "hello";
+	}
+
+	@PostMapping({"/authenticate"})
+	public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest)
+			throws Exception {
+
+		try {
+			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+					authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+		} catch (BadCredentialsException e) {
+			throw new Exception("Incorrect username or password", e);
+		}
+		final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+
+		final String jwt = jwtUtil.generateToken(userDetails);
+
+		return ResponseEntity.ok(new AuthenticationResponse(jwt));
+	}
+
+}
---
+++ demo-security-jwt/src/main/java/com/fazal/demo/hello/model/AuthenticationResponse.java
@@ -0,0 +1,16 @@
+package com.fazal.demo.hello.model;
+
+public class AuthenticationResponse {
+	
+	private final String jwt; 
+	
+	public AuthenticationResponse(String jwt) {
+		this.jwt=jwt;
+	}
+	
+	public String getJwt() {
+		return jwt;
+	}
+	
+
+}
